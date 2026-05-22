cdae1be48171c90b9caa6060e40cbcc9bb79f3bf
quentin.weber <quentin.weber@ost.ch>
2026-05-22 11:09:27 +0200

    fix serial port error on package

--- src/extension.ts
+++ src/extension.ts
@@ -51,7 +51,7 @@ export function activate(context: vscode.ExtensionContext): void {
           );
         }
       });
-    return; // stop activation
+    // return;
   }
   ensurePythonExtension();
 
